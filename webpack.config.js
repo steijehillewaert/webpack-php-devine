7dee04257ef0a37385530b2f5940d89b8fd1c047
steijehillewaert <info@steije.be>
2018-10-29 23:54:49 +0100

    Updated with latest webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const webpack = require(`webpack`);
 const port = 3000;
 
 const PATHS = {
-  src : path.join(__dirname, `src`),
+  src: path.join(__dirname, `src`),
   dist: path.join(__dirname, `dist`)
 };
 
@@ -31,13 +31,12 @@ const commonConfig = {
     path: PATHS.dist,
     filename: `js/script.js`,
   },
+  mode: `development`,
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: [
-          {
+        use: [{
             loader: `babel-loader`
           },
           {
@@ -50,36 +49,34 @@ const commonConfig = {
       },
       {
         test: /\.(jpe?g|png|gif|webp|svg)$/,
-        use:[
-          {
-            loader: `file-loader`,
-            options: {
-              limit: 1000,
-              context: `./src`,
-              name: `[path][name].[ext]`
-            }
-          },{
-            loader: `image-webpack-loader`,
-            options: {
-              bypassOnDebug: true,
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
+        use: [{
+          loader: `file-loader`,
+          options: {
+            limit: 1000,
+            context: `./src`,
+            name: `[path][name].[ext]`
+          }
+        }, {
+          loader: `image-webpack-loader`,
+          options: {
+            bypassOnDebug: true,
+            mozjpeg: {
+              progressive: true,
+              quality: 65
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
             },
           },
-        ]
+        }, ]
       }
     ]
   },
@@ -96,7 +93,7 @@ const productionConfig = merge([
   {
     plugins: [
       new ImageminPlugin({
-        test: /\.(jpe?g)$/i ,
+        test: /\.(jpe?g)$/i,
         plugins: [
           imageminJpegRecompress({})
         ]
@@ -105,8 +102,7 @@ const productionConfig = merge([
   }
 ]);
 
-const developmentConfig = merge([
-  {
+const developmentConfig = merge([{
     devServer: {
       overlay: true,
       contentBase: PATHS.src
